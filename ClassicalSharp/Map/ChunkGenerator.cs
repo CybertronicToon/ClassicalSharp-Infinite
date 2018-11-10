9838ffb1e929b5553d78e676c8139d59dfcde84b
CybertronicToon <rexyrexy2@gmail.com>
2018-11-10 03:46:01 -0600

    Upload part 2.

---
+++ ClassicalSharp/Map/ChunkGenerator.cs
@@ -0,0 +1,248 @@
+﻿// Copyright 2014-2017 ClassicalSharp | Licensed under BSD-3
+using System;
+using ClassicalSharp.Events;
+using ClassicalSharp.Renderers;
+using ClassicalSharp.Generator;
+using OpenTK;
+using BlockID = System.UInt16;
+using BlockRaw = System.Byte;
+
+namespace ClassicalSharp.Map {
+	public class ChunkGenerator {
+		
+		Game game;
+		
+		public long Seed;
+		
+		long chunkXMul, chunkZMul, chunkSeed;
+		
+		const short waterLevel = 64;
+		const int Height = 128;
+		const int oneY = 16 * 16;
+		
+		int minHeight;
+		
+		public JavaRandom rnd;
+		
+		ChunkHandler ChunkHandler;
+		
+		public ChunkGenerator(ChunkHandler ChunkHandler) {
+			rnd = new JavaRandom();
+			this.Seed = rnd.GetSeed();
+			//chunkXMul = rnd.nextLong();
+			//chunkZMul = rnd.nextLong();
+			
+			n1 = new CombinedNoise(
+				new OctaveNoise(8, rnd), new OctaveNoise(8, rnd));
+			n2 = new CombinedNoise(
+				new OctaveNoise(8, rnd), new OctaveNoise(8, rnd));
+			n3 = new OctaveNoise(6, rnd);
+			
+			n4 = new OctaveNoise(8, rnd);
+			
+			n5 = new OctaveNoise(8, rnd);
+			n6 = new OctaveNoise(8, rnd);
+			
+			this.ChunkHandler = ChunkHandler;
+		}
+		
+		public ChunkGenerator(ChunkHandler ChunkHandler, long Seed) {
+			rnd = new JavaRandom();
+			rnd.SetSeed(Seed);
+			this.Seed = Seed;
+			//chunkXMul = rnd.nextLong();
+			//chunkZMul = rnd.nextLong();
+			
+			n1 = new CombinedNoise(
+				new OctaveNoise(8, rnd), new OctaveNoise(8, rnd));
+			n2 = new CombinedNoise(
+				new OctaveNoise(8, rnd), new OctaveNoise(8, rnd));
+			n3 = new OctaveNoise(6, rnd);
+			
+			n4 = new OctaveNoise(8, rnd);
+			
+			n5 = new OctaveNoise(8, rnd);
+			n6 = new OctaveNoise(8, rnd);
+			
+			this.ChunkHandler = ChunkHandler;
+		}
+		
+		CombinedNoise n1, n2;
+		
+		OctaveNoise n3, n4, n5, n6;
+		
+		short[] heightmap;
+		
+		public void GenerateChunk(ref Chunk chunk, int chunkX, int chunkZ) {
+			chunkSeed = (chunkX * chunkXMul) ^ (chunkZ * chunkZMul) ^ Seed;
+			
+			heightmap = new short[16*16];
+			minHeight = Height;
+			
+			CreateHeightmap(ref chunk, chunkX, chunkZ, n1, n2, n3);
+			CreateStrata(ref chunk, chunkX, chunkZ, n4);
+			FloodFillWater(ref chunk);
+			CreateSurfaceLayer(ref chunk, chunkX, chunkZ, n5, n6);
+			
+		}
+		
+		void CreateHeightmap(ref Chunk chunk, int chunkX, int chunkZ, CombinedNoise n1, CombinedNoise n2, OctaveNoise n3) {
+			/*CombinedNoise n1 = new CombinedNoise(
+				new OctaveNoise(8, rnd), new OctaveNoise(8, rnd));
+			CombinedNoise n2 = new CombinedNoise(
+				new OctaveNoise(8, rnd), new OctaveNoise(8, rnd));
+			OctaveNoise n3 = new OctaveNoise(6, rnd);*/
+			int index = 0;
+			//short[] hMap = new short[16 * 16];
+			int offsetX = (chunkX * 16);
+			int offsetZ = (chunkZ * 16);
+			
+			for (int z = 0; z < 16; z++) {
+				int zCur = z + offsetZ;
+				for (int x = 0; x < 16; x++) {
+					int xCur = x + offsetX;
+					double hLow = n1.Compute(xCur * 1.3f, zCur * 1.3f) / 6 - 4, height = hLow;
+					
+					if (n3.Compute(xCur, zCur) <= 0) {
+						double hHigh = n2.Compute(xCur * 1.3f, zCur * 1.3f) / 5 + 6;
+						height = Math.Max(hLow, hHigh);
+					}
+					
+					height *= 0.5;
+					if (height < 0) height *= 0.8f;
+					
+					short adjHeight = (short)(height + waterLevel);
+					minHeight = adjHeight < minHeight ? adjHeight : minHeight;
+					index = z * 16 + x;
+					heightmap[index] = adjHeight;
+				}
+			}
+			//heightmap = hMap;
+		}
+		
+		void CreateStrata(ref Chunk chunk, int chunkX, int chunkZ, OctaveNoise n) {
+			int hMapIndex = 0, maxY = Height - 1, mapIndex = 0;
+			// Try to bulk fill bottom of the map if possible
+			int minStoneY = CreateStrataFast(ref chunk, chunkX, chunkZ);
+			int offsetX = (chunkX * 16);
+			int offsetZ = (chunkZ * 16);
+
+			for (int z = 0; z < 16; z++) {
+				int zCur = z + offsetZ;
+				for (int x = 0; x < 16; x++) {
+					int xCur = x + offsetX;
+					int index = z * 16 + x;
+					int dirtThickness = (int)(n.Compute(x, z) / 24 - 4);
+					int dirtHeight = heightmap[index];
+					int stoneHeight = dirtHeight + dirtThickness;	
+					
+					stoneHeight = Math.Min(stoneHeight, maxY);
+					dirtHeight  = Math.Min(dirtHeight,  maxY);
+					
+					mapIndex = minStoneY * oneY + z * 16 + x;
+					for (int y = minStoneY; y <= stoneHeight; y++) {
+						chunk.blocks[mapIndex] = Block.Stone; mapIndex += oneY;
+					}
+					
+					stoneHeight = Math.Max(stoneHeight, 0);
+					mapIndex = (stoneHeight + 1) * oneY + z * 16 + x;
+					for (int y = stoneHeight + 1; y <= dirtHeight; y++) {
+						chunk.blocks[mapIndex] = Block.Dirt; mapIndex += oneY;
+					}
+				}
+			}
+		}
+		
+		int CreateStrataFast(ref Chunk chunk, int chunkX, int chunkZ) {
+			// Make lava layer at bottom
+			int index = 0;
+			int offsetX = (chunkX * 16);
+			int offsetZ = (chunkZ * 16);
+			for (int z = 0; z < 16; z++)
+				for (int x = 0; x < 16; x++)
+			{
+				index = z * 16 + x;
+				chunk.blocks[index] = Block.Lava;
+			}
+			
+			// Invariant: the lowest value dirtThickness can possible be is -14
+			int stoneHeight = minHeight - 14;
+			if (stoneHeight <= 0) return 1; // no layer is fully stone
+			
+			// We can quickly fill in bottom solid layers
+			for (int y = 1; y <= stoneHeight; y++)
+				for (int z = 0; z < 16; z++)
+					for (int x = 0; x < 16; x++)
+			{
+				index = (y * 16 + z) * 16 + x;
+				chunk.blocks[index] = Block.Stone;
+			}
+			return stoneHeight;
+		}
+		
+		void FloodFillWater(ref Chunk chunk) {
+			int waterY = waterLevel - 1;
+			int index1 = (waterY * 16 + 0) * 16 + 0;
+			int index2 = (waterY * 16 + (16 - 1)) * 16 + 0;
+			
+			for (int x = 0; x < 16; x++) {
+				FloodFill(index1, Block.Water, chunk);
+				FloodFill(index2, Block.Water, chunk);
+				index1++; index2++;
+			}
+			
+			index1 = (waterY * 16 + 0) * 16 + 0;
+			index2 = (waterY * 16 + 0) * 16 + (16 - 1);
+			for (int z = 0; z < 16; z++) {
+				FloodFill(index1, Block.Water, chunk);
+				FloodFill(index2, Block.Water, chunk);
+				index1 += 16; index2 += 16;
+			}
+		}
+		
+		void FloodFill(int startIndex, BlockRaw block, Chunk chunk) {
+			if (startIndex < 0) return; // y below map, immediately ignore
+			Generator.NotchyGenerator.FastIntStack stack = new Generator.NotchyGenerator.FastIntStack(4);
+			stack.Push(startIndex);
+			
+			while (stack.Size > 0) {
+				int index = stack.Pop();
+				if (chunk.blocks[index] != Block.Air) continue;
+				chunk.blocks[index] = block;
+				
+				int x = index % 16;
+				int y = index / oneY;
+				int z = (index / 16) % 16;
+				
+				if (x > 0) stack.Push(index - 1);
+				if (x < 16 - 1) stack.Push(index + 1);
+				if (z > 0) stack.Push(index - 16);
+				if (z < 16 - 1) stack.Push(index + 16);
+				if (y > 0) stack.Push(index - oneY);
+			}
+		}
+		
+		void CreateSurfaceLayer(ref Chunk chunk, int chunkX, int chunkZ, OctaveNoise n1, OctaveNoise n2) {
+			// TODO: update heightmap
+			int offsetX = (chunkX * 16);
+			int offsetZ = (chunkZ * 16);
+			for (int z = 0; z < 16; z++) {
+				int zCur = z + offsetZ;
+				for (int x = 0; x < 16; x++) {
+					int xCur = x + offsetX;
+					int hMapIndex = z * 16 + x;
+					int y = heightmap[hMapIndex];
+					if (y < 0 || y >= Height) continue;
+					
+					int index = (y * 16 + z) * 16 + x;
+					BlockRaw blockAbove = y >= (Height - 1) ? Block.Air : chunk.blocks[index + oneY];
+					if (blockAbove == Block.Water && (n2.Compute(xCur, zCur) > 12)) {
+						chunk.blocks[index] = Block.Gravel;
+					} else if (blockAbove == Block.Air) {
+						chunk.blocks[index] = (y <= waterLevel && (n1.Compute(xCur, zCur) > 8)) ? Block.Sand : Block.Grass;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ ClassicalSharp/Map/ChunkHandler.cs
@@ -0,0 +1,340 @@
+﻿// Copyright 2014-2017 ClassicalSharp | Licensed under BSD-3
+using System;
+using ClassicalSharp.Events;
+using ClassicalSharp.Renderers;
+using OpenTK;
+using BlockID = System.UInt16;
+using BlockRaw = System.Byte;
+
+namespace ClassicalSharp.Map {
+	public class ChunkHandler {
+		
+		Game game;
+		
+		public Chunk[,] ChunkArray;
+		
+		public int curChunkX;
+		public int curChunkY;
+		
+		public ChunkGenerator ChunkGenerator;
+		
+		public ChunkHandler(int size, Game game) {
+			this.game = game;
+			ChunkArray = new Chunk[size, size];
+			ChunkGenerator = new ChunkGenerator(this);
+		}
+		
+		public ChunkHandler(BlockRaw[] blocks, int size, Game game) {
+			this.game = game;
+			ChunkGenerator = new ChunkGenerator(this);
+			
+			int numChunks = size / 16;
+			ChunkArray = new Chunk[numChunks, numChunks];
+			for (int x = 0; x < numChunks; x++)
+				for (int y = 0; y < numChunks; y++) {
+				int chunkAdjX = (x * 16);
+				int chunkAdjY = (y * 16);
+				Chunk newChunk = new Chunk(x, y);
+				for (int bX = 0; bX < 16; bX++)
+					for (int bY = 0; bY < 128; bY++)
+						for(int bZ = 0; bZ < 16; bZ++) {
+					newChunk.blocks[(bY * 16 + bZ) * 16 + bX] = blocks[(bY * size + (bZ + chunkAdjY)) * size + (bX + chunkAdjX)];
+				}
+				ChunkArray[y, x] = newChunk;
+			}
+		}
+		
+		public void UpdateCurChunk() {
+			int oldChunkX = curChunkX;
+			int oldChunkY = curChunkY;
+			Vector3 curPos = game.LocalPlayer.Position;
+			int x = Utils.Floor(curPos.X);
+			int y = Utils.Floor(curPos.Z);
+			bool isNegX = false;
+			bool isNegY = false;
+			if (x < 0) {
+				x -= 1;
+				isNegX = true;
+			}
+			if (y < 0) {
+				y -= 1;
+				isNegY = true;
+			}
+			curChunkX = x / 16;
+			if (isNegX) curChunkX -= 1;
+			curChunkY = y / 16;
+			if (isNegY) curChunkY -= 1;
+			if (oldChunkX != curChunkX || oldChunkY != curChunkY) {
+				AdjChunks(oldChunkX, oldChunkY);
+			}
+		}
+		
+		public void AdjChunks(int oldChunkX, int oldChunkY) {
+			int length = ChunkArray.GetLength(0);
+			bool xNeg = false;
+			bool yNeg = false;
+			if (oldChunkX > curChunkX) xNeg = true;
+			if (oldChunkY > curChunkY) yNeg = true;
+			int adjX = curChunkX - oldChunkX;
+			int adjY = curChunkY - oldChunkY;
+			int curX = 0;
+			if (xNeg) curX = length - 1;
+			int curY = 0;
+			if (yNeg) curY = length - 1;
+			for (int x = 0; x < length; x++) {
+				for (int y = 0; y < length; y++) {
+					int newX = curX - adjX;
+					int newY = curY - adjY;
+					if (newX > 0 && newY > 0 && newX < length && newY < length) {
+						Chunk chunk = ChunkArray[ curY, curX];
+						ChunkArray[newY, newX] = chunk;
+						ChunkArray[curY, curX] = null;
+					} else {
+						ChunkArray[curY, curX] = null;
+					}
+					if (yNeg) {
+						curY -= 1;
+					} else {
+						curY += 1;
+					}
+				}
+				if (xNeg) {
+					curX -= 1;
+				} else {
+					curX += 1;
+				}
+				curY = 0;
+				if (yNeg) curY = length - 1;
+			}
+			
+			GenNullChunks();
+			game.MapRenderer.Refresh();
+			//game.MapRenderer.AdjChunks(oldChunkX, oldChunkY);
+			game.Lighting.Refresh();
+		}
+		
+		public void GenNullChunks() {
+			int length = ChunkArray.GetLength(0);
+			
+			for (int x = 0; x < length; x++)
+				for (int y = 0; y < length; y++) {
+				int chunkX = x + (curChunkX - 4);
+				int chunkY = y + (curChunkY - 4);
+				if (ChunkArray[y, x] != null) continue;
+				Chunk chunk = new Chunk(chunkX, chunkY);
+				ChunkGenerator.GenerateChunk(ref chunk, chunkX, chunkY);
+				ChunkArray[y, x] = chunk;
+			}
+		}
+		
+		public void SetNewMap(BlockRaw[] blocks, int size) {
+			
+			Console.WriteLine("new ChunkHandler");
+			
+			int numChunks = size / 16;
+			ChunkArray = new Chunk[numChunks, numChunks];
+			for (int x = 0; x < numChunks; x++)
+				for (int y = 0; y < numChunks; y++) {
+				int chunkAdjX = (x * 16);
+				int chunkAdjY = (y * 16);
+				Chunk newChunk = new Chunk(x, y);
+				for (int bX = 0; bX < 16; bX++)
+					for (int bY = 0; bY < 128; bY++)
+						for(int bZ = 0; bZ < 16; bZ++) {
+					newChunk.blocks[(bY * 16 + bZ) * 16 + bX] = blocks[(bY * size + (bZ + chunkAdjY)) * size + (bX + chunkAdjX)];
+				}
+				ChunkArray[y, x] = newChunk;
+			}
+		}
+		
+		public void GenInitialChunks(long seed, int size) {
+			
+			Console.WriteLine("new ChunkHandler");
+			
+			ChunkGenerator = new ChunkGenerator(this, seed);
+			
+			int numChunks = size / 16;
+			ChunkArray = new Chunk[numChunks, numChunks];
+			for (int x = 0; x < numChunks; x++)
+				for (int y = 0; y < numChunks; y++) {
+				int chunkAdjX = (x * 16);
+				int chunkAdjY = (y * 16);
+				Chunk newChunk = new Chunk(x, y);
+				ChunkGenerator.GenerateChunk(ref newChunk, newChunk.x, newChunk.y);
+				/*for (int bX = 0; bX < 16; bX++)
+					for (int bY = 0; bY < 128; bY++)
+						for(int bZ = 0; bZ < 16; bZ++) {
+					newChunk.blocks[(bY * 16 + bZ) * 16 + bX] = blocks[(bY * size + (bZ + chunkAdjY)) * size + (bX + chunkAdjX)];
+				}*/
+				ChunkArray[y, x] = newChunk;
+			}
+		}
+		
+		public BlockRaw GetBlock(int x, int y, int z) {
+			if (!inMap(x, y, z)) return Block.Air;
+			int chunkX = x / 16;
+			if (x < 0) chunkX -= 1;
+			int blockX = x % 16;
+			int chunkY = z / 16;
+			if (z < 0) chunkY -= 1;
+			int blockZ = z % 16;
+			int blockY = y;
+			if (blockY < 0) return Block.Lava;
+			if (blockY > 127) return Block.Air;
+			return ChunkArray[chunkY, chunkX].blocks[(blockY * 16 + blockZ) * 16 + blockX];
+		}
+		
+		public BlockRaw GetBlockAdj(int x, int y, int z) {
+			if (!inMap(x, y, z)) return Block.Air;
+			//int chunkX = x / 16;
+			int chunkX = x >> 4;
+			//int blockX = x % 16;
+			int blockX = x & 15;
+			//if (x < 0) chunkX -= 1;
+			//int chunkY = z / 16;
+			int chunkY = z >> 4;
+			//if (z < 0) chunkY -= 1;
+			//int blockZ = z % 16
+			int blockZ = z & 15;
+			int blockY = y;
+			if (blockY < 0) return Block.Lava;
+			if (blockY > 127) return Block.Air;
+			return ChunkArray[chunkY - (curChunkY - 4), chunkX - (curChunkX - 4)].blocks[(blockY * 16 + blockZ) * 16 + blockX];
+		}
+		
+		public BlockRaw GetBlockAdjSafe(int x, int y, int z) {
+			if (!inMap(x, y, z)) return Block.Air;
+			//int chunkX = x / 16;
+			int chunkX = x >> 4;
+			//int blockX = x % 16;
+			int blockX = x & 15;
+			//if (x < 0) chunkX -= 1;
+			//int chunkY = z / 16;
+			int chunkY = z >> 4;
+			//if (z < 0) chunkY -= 1;
+			//int blockZ = z % 16
+			int blockZ = z & 15;
+			int blockY = y;
+			if (blockY < 0) return Block.Lava;
+			if (blockY > 127) return Block.Air;
+			if (chunkX - (curChunkX - 4) < 0 || chunkY - (curChunkY - 4) < 0) {
+				return Block.Air;
+			} else if (chunkX - (curChunkX - 4) >= ChunkArray.GetLength(0) ||
+			           chunkY - (curChunkY - 4) >= ChunkArray.GetLength(0)) {
+				return Block.Air;
+			}
+			return ChunkArray[chunkY - (curChunkY - 4), chunkX - (curChunkX - 4)].blocks[(blockY * 16 + blockZ) * 16 + blockX];
+		}
+		
+		public void SetBlock(int x, int y, int z, BlockRaw blockId) {
+			int chunkX = x / 16;
+			if (x < 0) chunkX -= 1;
+			int blockX = x % 16;
+			int chunkY = z / 16;
+			if (z < 0) chunkY -= 1;
+			int blockZ = z % 16;
+			int blockY = y;
+			ChunkArray[chunkY, chunkX].blocks[(blockY * 16 + blockZ) * 16 + blockX] = blockId;
+		}
+		
+		public void SetBlockAdj(int x, int y, int z, BlockRaw blockId) {
+			//int chunkX = x / 16;
+			int chunkX = x >> 4;
+			//int blockX = x % 16;
+			int blockX = x & 15;
+			//if (x < 0) chunkX -= 1;
+			//int chunkY = z / 16;
+			int chunkY = z >> 4;
+			//if (z < 0) chunkY -= 1;
+			//int blockZ = z % 16
+			int blockZ = z & 15;
+			int blockY = y;
+			ChunkArray[chunkY - (curChunkY - 4), chunkX - (curChunkX - 4)].blocks[(blockY * 16 + blockZ) * 16 + blockX] = blockId;
+		}
+		
+		public BlockRaw GetBlockUnsafe(int x, int y, int z) {
+			int chunkX = x / 16;
+			int blockX = x % 16;
+			if (x < 0) chunkX -= 1;
+			int chunkY = z / 16;
+			if (z < 0) chunkY -= 1;
+			int blockZ = z % 16;
+			int blockY = y;
+			if (blockY < 0) return Block.Lava;
+			if (blockY > 127) return Block.Air;
+			return ChunkArray[chunkY, chunkX].blocks[(blockY * 16 + blockZ) * 16 + blockX];
+		}
+		
+		public BlockRaw GetBlockAdjUnsafe(int x, int y, int z) {
+			//int chunkX = x / 16;
+			int chunkX = x >> 4;
+			//int blockX = x % 16;
+			int blockX = x & 15;
+			//if (x < 0) chunkX -= 1;
+			//int chunkY = z / 16;
+			int chunkY = z >> 4;
+			//if (z < 0) chunkY -= 1;
+			//int blockZ = z % 16
+			int blockZ = z & 15;
+			int blockY = y;
+			if (blockY < 0) return Block.Lava;
+			if (blockY > 127) return Block.Air;
+			return ChunkArray[chunkY - (curChunkY - 4), chunkX - (curChunkX - 4)].blocks[(blockY * 16 + blockZ) * 16 + blockX];
+		}
+		
+		public void GetChunkCoords(int x, int y, out int chunkX, out int chunkY) {
+			chunkX = x / 16;
+			if (x < 0) chunkX -= 1;
+			chunkY = y / 16;
+			if (y < 0) chunkY -= 1;
+		}
+		
+		public void GetChunkCoords(Vector3 coords, out int chunkX, out int chunkY) {
+			chunkX = (int)coords.X / 16;
+			if (coords.X < 0) chunkX -= 1;
+			chunkY = (int)coords.Z / 16;
+			if (coords.Z < 0) chunkY -= 1;
+		}
+		
+		public void GetChunkCoords(Vector3I coords, out int chunkX, out int chunkY) {
+			chunkX = coords.X / 16;
+			if (coords.X < 0) chunkX -= 1;
+			chunkY = coords.Z / 16;
+			if (coords.Z < 0) chunkY -= 1;
+		}
+		
+		public bool inMap(int x, int y, int z) {
+			int numBlocks = (ChunkArray.GetLength(1) * 16) - 1;
+			if (x > numBlocks || x < 0) return false;
+			if (y > 127 || y < 0) return false;
+			if (z > numBlocks || z < 0) return false;
+			return true;
+		}
+	}
+	
+	public class Chunk {
+		public Chunk(int x, int y) {
+			blocks = new BlockRaw[16 * 16 * 128];
+			this.x = x;
+			this.y = y;
+		}
+		
+		public int x, y;
+		
+		public BlockRaw[] blocks;
+		public byte[] lightMap;
+	}
+	
+	public class ChunkLoc {
+		///<summary>Chunk X.</summary>
+		public int CX;
+		///<summary>Chunk Y.</summary>
+		public int CY;
+		///<summary>Block X.</summary>
+		public int BX;
+		///<summary>Block Y.</summary>
+		public int BY;
+		/// <summary>Block Z.</summary>
+		public int BZ;
+	}
+}
